e4a40c592d6d5de709d633076d5e9b30cf66df19
Francesco Conforti <82725386+checcoconf@users.noreply.github.com>
2022-02-18 22:10:01 +0100

    Update Sensore Temperatura Umidità monitor lcd.ino

--- Sensore Temperatura Umidità monitor lcd.ino
+++ Sensore Temperatura Umidità monitor lcd.ino
@@ -1,7 +1,7 @@
 #include <DHT.h>
 #include <LiquidCrystal.h>
-#define DHTPIN 8      // 8 è il pin di Arduino a cui collego il sensore di temperatura
-#define DHTTYPE DHT11 // dht11 è il tipo di sensore che uso
+#define DHTPIN 8 // 8 è il pin di Arduino a cui collego il sensore di temperatura.
+#define DHTTYPE DHT11 // DHT11 è il tipo di sensore che uso.
 
 DHT dht(DHTPIN, DHTTYPE);
 #define RS 2
@@ -10,12 +10,12 @@ DHT dht(DHTPIN, DHTTYPE);
 #define D5 5
 #define D6 6
 #define D7 7
-LiquidCrystal lcd(RS, EN, D4, D5, D6, D7); // connessione display (pin)
+LiquidCrystal lcd(RS, EN, D4, D5, D6, D7); // connessione display una vola definiti i pin.
 
 void setup()
 {
   lcd.begin(16, 2); // Definisci in pixel larghezza e altezza del tuo schermo.
-  dht.begin();      // Attivo il sensore.
+  dht.begin(); // Attivo il sensore.
 }
 void loop()
 {
@@ -23,9 +23,9 @@ void loop()
   int h = dht.readHumidity();
   lcd.clear();
 
-  lcd.setCursor(0, 0); // imposto la riga dove stamperà il dato
+  lcd.setCursor(0, 0); // imposto la riga dove stamperà il dato.
   lcd.print(" Gradi: " + String(t) + char(0xDF) + "C");
-  lcd.setCursor(0, 1); // imposto la riga dove stamperà il dato
+  lcd.setCursor(0, 1); // imposto la riga dove stamperà il dato.
   lcd.print(" Umidita': " + String(h) + "%");
-  delay(1000); // Attendi 1000 ms per poter raggiornare i dati
+  delay(1000); // Attendi 1000 ms per poter riaggiornare i dati.
 }
